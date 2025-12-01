c98ec732dfee17971a93ed156eb5f51830fa7278
Yuhei Yamada <pepushi514@gmail.com>
2025-12-01 23:44:54 +0900

    fix: backend依存のものをbackendディレクトリに移動

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,8 @@ import uvicorn
 
 
 def main() -> None:
-    uvicorn.run("backend.app.main:app", host="0.0.0.0", port=8000, reload=True)
+    # backend ディレクトリ内で実行することを想定し、モジュール指定を app.* に統一
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
 
 
 if __name__ == "__main__":
